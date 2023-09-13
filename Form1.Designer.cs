5e5ae020d69a46dd3747d58d0e50c37300ae8959
Iceirix <luisfdopapu@gmail.com>
2023-09-13 12:56:44 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace FormComplejosG2_2024_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbImagnario1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbOperacion = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbReal1 = new System.Windows.Forms.TextBox();
+            this.txtbImagnario2 = new System.Windows.Forms.TextBox();
+            this.txtbReal2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbSuma = new System.Windows.Forms.Label();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnConjugado = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbImagnario1
+            // 
+            this.txtbImagnario1.Location = new System.Drawing.Point(179, 75);
+            this.txtbImagnario1.Name = "txtbImagnario1";
+            this.txtbImagnario1.Size = new System.Drawing.Size(56, 20);
+            this.txtbImagnario1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(241, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "i";
+            // 
+            // lbOperacion
+            // 
+            this.lbOperacion.AutoSize = true;
+            this.lbOperacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOperacion.Location = new System.Drawing.Point(261, 73);
+            this.lbOperacion.Name = "lbOperacion";
+            this.lbOperacion.Size = new System.Drawing.Size(21, 24);
+            this.lbOperacion.TabIndex = 1;
+            this.lbOperacion.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Complejo2";
+            // 
+            // txtbReal1
+            // 
+            this.txtbReal1.Location = new System.Drawing.Point(99, 75);
+            this.txtbReal1.Name = "txtbReal1";
+            this.txtbReal1.Size = new System.Drawing.Size(56, 20);
+            this.txtbReal1.TabIndex = 0;
+            // 
+            // txtbImagnario2
+            // 
+            this.txtbImagnario2.Location = new System.Drawing.Point(371, 75);
+            this.txtbImagnario2.Name = "txtbImagnario2";
+            this.txtbImagnario2.Size = new System.Drawing.Size(56, 20);
+            this.txtbImagnario2.TabIndex = 0;
+            // 
+            // txtbReal2
+            // 
+            this.txtbReal2.Location = new System.Drawing.Point(291, 75);
+            this.txtbReal2.Name = "txtbReal2";
+            this.txtbReal2.Size = new System.Drawing.Size(56, 20);
+            this.txtbReal2.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(433, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 24);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "i";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(453, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 24);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Complejo2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Complejo1";
+            // 
+            // lbSuma
+            // 
+            this.lbSuma.AutoSize = true;
+            this.lbSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSuma.Location = new System.Drawing.Point(480, 75);
+            this.lbSuma.Name = "lbSuma";
+            this.lbSuma.Size = new System.Drawing.Size(39, 20);
+            this.lbSuma.TabIndex = 1;
+            this.lbSuma.Text = "0+0i";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(99, 130);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(75, 49);
+            this.btnSuma.TabIndex = 2;
+            this.btnSuma.Text = "Sumar";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnConjugado
+            // 
+            this.btnConjugado.Location = new System.Drawing.Point(280, 130);
+            this.btnConjugado.Name = "btnConjugado";
+            this.btnConjugado.Size = new System.Drawing.Size(75, 49);
+            this.btnConjugado.TabIndex = 2;
+            this.btnConjugado.Text = "Conjugar";
+            this.btnConjugado.UseVisualStyleBackColor = true;
+            this.btnConjugado.Click += new System.EventHandler(this.btnConjugado_Click);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Location = new System.Drawing.Point(371, 130);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(75, 49);
+            this.btnMultiplicacion.TabIndex = 2;
+            this.btnMultiplicacion.Text = "Multiplicar";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Location = new System.Drawing.Point(190, 130);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(75, 49);
+            this.btnResta.TabIndex = 2;
+            this.btnResta.Text = "Restar";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 245);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnConjugado);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbOperacion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbSuma);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbReal2);
+            this.Controls.Add(this.txtbReal1);
+            this.Controls.Add(this.txtbImagnario2);
+            this.Controls.Add(this.txtbImagnario1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtbImagnario1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbOperacion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbReal1;
+        private System.Windows.Forms.TextBox txtbImagnario2;
+        private System.Windows.Forms.TextBox txtbReal2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbSuma;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnConjugado;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnResta;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FormComplejosG2_2024_1
+{
+    public partial class Form1 : Form
+    {
+        Complejo c1;
+        Complejo c2;
+        Complejo c3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbReal1.Text), float.Parse(txtbImagnario1.Text));
+            c2 = new Complejo(float.Parse(txtbReal2.Text), float.Parse(txtbImagnario2.Text));
+            c3 = c1 + c2;
+            lbSuma.Text = c3.ToString();
+            lbOperacion.Text = "+";
+        }
+        private void btnConjugado_Click(object sender, EventArgs e)
+        {
+            lbSuma.Text = (~c3).ToString();
+        }
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbReal1.Text), float.Parse(txtbImagnario1.Text));
+            c2 = new Complejo(float.Parse(txtbReal2.Text), float.Parse(txtbImagnario2.Text));
+            c3 = c1 - c2;
+            lbSuma.Text = c3.ToString();
+            lbOperacion.Text = "-";
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            c1 = new Complejo(float.Parse(txtbReal1.Text), float.Parse(txtbImagnario1.Text));
+            c2 = new Complejo(float.Parse(txtbReal2.Text), float.Parse(txtbImagnario2.Text));
+            c3 = c1 * c2;
+            lbSuma.Text = c3.ToString();
+            lbOperacion.Text = "*";
+        }
+    }
+}
